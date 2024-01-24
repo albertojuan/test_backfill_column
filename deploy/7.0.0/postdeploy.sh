2d3d97850da26b0112766aec31de9e676c5bdb48
Alberto Juan <alberto@tinybird.co>
2024-01-24 17:06:59 +0100

    Try populate strategy

--- deploy/7.0.0/postdeploy.sh
+++ deploy/7.0.0/postdeploy.sh
@@ -1,3 +1,4 @@
 set -e
 
-tb --semver 7.0.0 pipe populate v600_to_v700 --node live_to_new --sql-condition "1=1" --wait
+tb --semver 7.0.0 pipe populate v600_to_v700 --node live_to_new --wait
+tb --semver 7.0.0 pipe populate v600_to_v700 --node live_to_new --wait --sql-condition "1=1"
