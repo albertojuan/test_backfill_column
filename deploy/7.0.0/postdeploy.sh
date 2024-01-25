bcb1baa55d6bb970e3602d222783b469e6060e4e
Alberto Juan <alberto@tinybird.co>
2024-01-25 09:55:15 +0100

    Populate all

--- deploy/7.0.0/postdeploy.sh
+++ deploy/7.0.0/postdeploy.sh
@@ -1,3 +1,6 @@
 set -e
 
 tb --semver 7.0.0 pipe populate v600_to_v700 --node live_to_new --wait
+tb --semver 7.0.0 pipe populate analytics_pages --node analytics_pages_1 --wait
+tb --semver 7.0.0 pipe populate analytics_sessions --node analytics_sessions_1 --wait
+tb --semver 7.0.0 pipe populate analytics_sources --node analytics_sources_1 --wait
